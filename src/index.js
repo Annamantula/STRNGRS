1d66d54a1b13437d7e85c3f976d1a0336c419dec
Anna Mantula <ganna.mantula@gmail.com>
2022-07-09 13:47:45 -0500

    7.9.22 1.47p

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import {App} from './components'
+import { 
+    BrowserRouter, 
+    Routes, 
+    Route 
+} from "react-router-dom";
+const root = ReactDOM.createRoot(
+    document.getElementById('app')
+  );
+  root.render(
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>
+  );
